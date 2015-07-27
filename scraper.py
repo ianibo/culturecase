9455864a1d718f0d5795c628431194b6ce07cd91
Ian Ibbotson <ian.ibbotson@k-int.com>
2015-07-27 19:40:41 +0100

    First phase of scraper

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
+import scraperwiki
+import time
+import requests
+import lxml.html
+
+
 # import scraperwiki
 # import lxml.html
 #
@@ -22,3 +28,18 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+html = scraperwiki.scrape("http://www.culturecase.org/contents/")
+
+root = lxml.html.fromstring(html)
+
+for el in root.cssselect("div.post-content p a"):
+    print el.text
+    print el.attrib['href']
+
+# Write out to the sqlite database using scraperwiki library
+# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+
+# for i in range(1, 6):
+#     print "%i..." % i
+#     time.sleep(1)
